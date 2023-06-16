31f32592f8dac4076dcb83356de8c7a01bda1dd5
Denis <Ori-wiki@gmail.com>
2023-06-16 20:21:54 +0300

    add comp AboutProject

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -1,3 +1,41 @@
-{
-  /* <SectionTitle id={"aboutProject"} text={"О проекте"} /> */
+import React from 'react';
+import './AboutProject.css';
+import SectionTitle from '../SectionTitle/SectionTitle';
+
+function AboutProject() {
+  return (
+    <section className='about'>
+      <SectionTitle id={'aboutProject'} text={'О проекте'} />
+      <div className='about__container'>
+        <div className='about__container-text'>
+          <h3 className='about__title'>Дипломный проект включал 5 этапов</h3>
+          <p className='about__text'>
+            Составление плана, работу над бэкендом, вёрстку, добавление
+            функциональности и финальные доработки.
+          </p>
+        </div>
+        <div className='about__container-text'>
+          <h3 className='about__title'>На выполнение диплома ушло 5 недель</h3>
+          <p className='about__text'>
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+            соблюдать, чтобы успешно защититься.
+          </p>
+        </div>
+      </div>
+      <div className='about__progress'>
+        <div className='about__progress-container'>
+          <span className='about__progress-time about__progress-time_green'>
+            1 неделя
+          </span>
+          <span className='about__progress-text'>Back-end</span>
+        </div>
+        <div className='about__progress-container'>
+          <span className='about__progress-time '>4 недели</span>
+          <span className='about__progress-text'>Front-end</span>
+        </div>
+      </div>
+    </section>
+  );
 }
+
+export default AboutProject;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,12 +3,14 @@ import { Link, NavLink, Route, Routes } from 'react-router-dom';
 import './Main.css';
 import Promo from '../Promo/Promo';
 import NavTab from '../NavTab/NavTab';
+import AboutProject from '../AboutProject/AboutProject';
 
 function Main() {
   return (
     <main className='main'>
       <Promo />
       <NavTab />
+      <AboutProject />
     </main>
   );
 }
