e771b8c313cfa915afdea322afa1efc30759aa6f
Denis <Ori-wiki@gmail.com>
2023-06-17 22:44:18 +0300

    add comp SearchForm

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -15,14 +15,17 @@ function SearchForm() {
         />
         <button className='search-form__button-submit'></button>
       </form>
-      <div className='search-form__box'>
-        <input
-          className='search-form__checkbox'
-          type='checkbox'
-          name='short-movies'
-          id='short-movies-checkbox'
-        />
-        <label className='seacrh-form__label'>Короткометражки</label>
+      <div className='search-form__block'>
+        <label className='seacrh-form__label'>
+          <input
+            className='search-form__checkbox'
+            type='checkbox'
+            name='short-movies'
+            id='short-movies-checkbox'
+          />
+          <span className='searh-from__box'></span>
+          Короткометражки
+        </label>
       </div>
     </section>
   );
