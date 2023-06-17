abccc184cca2336ff6d60d3dd7f1029f1a7ae434
Denis <Ori-wiki@gmail.com>
2023-06-17 10:17:45 +0300

    fix comp tech

--- src/components/Tech/Tech.jsx
+++ src/components/Tech/Tech.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Tech.css';
-import { Link } from 'react-router-dom';
 
 function Tech({ text, link }) {
   return (
-    <Link to={link} target='_blank' className='tech'>
-      <span className='tech__text'>{text}</span>
-    </Link>
+    <li className='tech'>
+      <a className='tech__text' href={link} rel='noreferrer' target='_blank'>
+        {text}
+      </a>
+    </li>
   );
 }
 
