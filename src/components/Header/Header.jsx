08c2f4941138723bac808677bcadbc6ea7a526f2
Denis <Ori-wiki@gmail.com>
2023-06-19 20:09:08 +0300

    add burger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,56 +1,25 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
 
 import './Header.css';
 
 import Logo from '../Logo/Logo';
+import Navigation from '../Navigation/Navigation';
+import BurgerMenu from '../BurgerMenu/BurgerMenu';
 
 function Header({ loggedIn = false }) {
   return !loggedIn ? (
     <header className='header header_logout'>
       <Logo />
-      <nav className='header__nav'>
-        <Link to='/sign-up' className={'header__link header__link_signup'}>
-          Регистрация
-        </Link>
-        <Link to='/sign-in' className={'header__link header__link_signin'}>
-          Войти
-        </Link>
-      </nav>
+      <Navigation loggedIn={loggedIn} />
     </header>
   ) : (
     <header className='header'>
-      <Link to='/'>
-        <Logo />
-      </Link>
-      <nav className='header__nav'>
-        <NavLink
-          to='/movies'
-          className={({ isActive }) =>
-            isActive ? 'header__link_active header__link' : 'header__link'
-          }
-        >
-          Фильмы
-        </NavLink>
-        <NavLink
-          to='/saved-movies'
-          className={({ isActive }) =>
-            isActive ? 'header__link_active header__link' : 'header__link'
-          }
-        >
-          Сохранённые фильмы
-        </NavLink>
-        <NavLink
-          className={({ isActive }) =>
-            isActive
-              ? 'header__box header__link header__link_active  '
-              : 'header__box header__link'
-          }
-          to='/profile'
-        >
-          Аккаунт<span className='header__account-icon'></span>
-        </NavLink>
-      </nav>
+      <Logo />
+      <label className='header__nav_label'>
+        <input type='checkbox' className=' header__nav_checkbox' />
+        <BurgerMenu />
+      </label>
+      <Navigation loggedIn={loggedIn} />
     </header>
   );
 }
