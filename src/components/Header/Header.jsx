9a14ed63c2442f788ea71215973920a475c51d80
Denis <Ori-wiki@gmail.com>
2023-06-22 00:50:09 +0300

    header color

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 import './Header.css';
 
@@ -7,13 +8,19 @@ import Navigation from '../Navigation/Navigation';
 import BurgerMenu from '../BurgerMenu/BurgerMenu';
 
 function Header({ loggedIn = false }) {
+  const location = useLocation();
+
   return !loggedIn ? (
-    <header className='header header_logout'>
+    <header className='header header_theme_dark'>
       <Logo />
       <Navigation loggedIn={loggedIn} />
     </header>
   ) : (
-    <header className='header'>
+    <header
+      className={`header header_theme_${
+        location.pathname === '/' ? 'dark' : 'light'
+      }`}
+    >
       <Logo />
       <label className='header__label'>
         <input type='checkbox' className='header__checkbox' />
