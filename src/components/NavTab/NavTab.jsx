7d369cd6d9f5d865dd514783a3caf2e6711814a6
Denis <Ori-wiki@gmail.com>
2023-06-22 00:10:16 +0300

    remove smooth

--- src/components/NavTab/NavTab.jsx
+++ src/components/NavTab/NavTab.jsx
@@ -4,13 +4,13 @@ import './NavTab.css';
 function NavTab() {
   return (
     <section className='nav'>
-      <a smooth='true' href={'#aboutProject'} className='nav__link'>
+      <a href={'#aboutProject'} className='nav__link'>
         О проекте
       </a>
-      <a smooth='true' href={'#techs'} className='nav__link'>
+      <a href={'#techs'} className='nav__link'>
         Технологии
       </a>
-      <a smooth='true' href={'#student'} className='nav__link'>
+      <a href={'#student'} className='nav__link'>
         Студент
       </a>
     </section>
