1211b60e15a8db07ed5eb1e99515923f342068e5
Denis <Ori-wiki@gmail.com>
2023-07-05 20:44:57 +0300

    фикс валидации email

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,9 +2,6 @@ import { useCallback, useState } from 'react';
 
 import isEmail from 'validator/es/lib/isEmail';
 
-const regex =
-  /^(http|https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*)?$/im;
-
 export function useFormWithValidation() {
   const [values, setValues] = useState({});
   const [errors, setErrors] = useState({});
@@ -15,17 +12,17 @@ export function useFormWithValidation() {
     const name = input.name;
     const value = input.value;
 
+    if (name === 'email') {
+      if (!isEmail(value)) {
+          input.setCustomValidity('Некорректый адрес почты.');
+      } else {
+          input.setCustomValidity('');
+      }
+    }
+
     setValues({ ...values, [name]: value });
     setErrors({ ...errors, [name]: input.validationMessage });
     setIsValid(input.closest('form').checkValidity());
-
-    if (name === 'email') {
-      if (!isEmail(value)) {
-        input.setCustomValidity('Некорректый адрес почты.');
-      } else {
-        input.setCustomValidity('');
-      }
-    }
   };
   const resetForm = useCallback(
     (newValues = {}, newErrors = {}, newIsValid = false) => {
@@ -47,5 +44,3 @@ export function useFormWithValidation() {
     setErrors,
   };
 }
-
-console.log(!regex.test('qwe@gmail.com'));
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -29,5 +29,4 @@ export const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 // export const MAIN_URL = 'https://api.muda.nomoredomains.rocks';
 export const MAIN_URL = 'http://localhost:3000';
 
-
-export const SHORTMOVIES_DURATION = 40
+export const SHORTMOVIES_DURATION = 40;
