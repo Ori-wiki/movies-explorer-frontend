235274e6e2d454a14e173a7ad7d87183bbb774c9
Denis <Ori-wiki@gmail.com>
2023-07-07 12:26:39 +0300

    url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -26,7 +26,7 @@ export const HEADER_ENDPOINTS = ['/', '/movies', '/saved-movies', '/profile'];
 export const FOOTER_ENDPOINTS = ['/', '/movies', '/saved-movies'];
 
 export const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
-// export const MAIN_URL = 'https://api.muda.nomoredomains.rocks';
-export const MAIN_URL = 'http://localhost:3000';
+export const MAIN_URL = 'https://api.muda.nomoredomains.rocks';
+// export const MAIN_URL = 'http://localhost:3000';
 
 export const SHORTMOVIES_DURATION = 40;
